a08b91ef7241bb7ff7bbe79354a5ab3ef3616994
Philip Fackler <facklerpw@ornl.gov>
2022-03-31 10:13:49 -0400

    Updated argument help

--- src/core/inputs.jl
+++ src/core/inputs.jl
@@ -12,11 +12,11 @@ function parse_inputs(args; error_handler = ArgParse.default_handler)
     )
     @add_arg_table! s begin
         "hello"
-        help = "Run in hello mode (minimal functionality test)"
+        help = """Run in "hello" mode (minimal functionality)"""
         action = :command
-        "generate-config"
-        help = """Create default config file
-        (as "config.yaml" unless --config option is used)"""
+        "generate-config", "gencfg"
+        help = """Create default configuration file
+        (as "$(default_config_filename())" unless --config option is used)"""
         action = :command
         "--config", "-c"
         help = "Specify name of (YAML) config file to generate I/O"
