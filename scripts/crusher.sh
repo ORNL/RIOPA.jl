8c5c8bab0d3f80d627186d7436cc4c44f79b1916
Philip Fackler <facklerpw@ornl.gov>
2022-09-16 13:29:31 -0400

    Added crusher setup script (#45)

--- scripts/crusher.sh
+++ scripts/crusher.sh
@@ -4,7 +4,7 @@ module load hdf5
 module load julia
 
 export JULIA_HDF5_PATH="$OLCF_HDF5_ROOT/bin"
-export JULIA_MPI_PATH=$OLCF_SPECTRUM_MPI_ROOT
+export JULIA_MPI_PATH=$MPICH_DIR
 export JULIA_MPI_BINARY=system
 
 julia --project -e 'using Pkg; Pkg.build(verbose=true)'
